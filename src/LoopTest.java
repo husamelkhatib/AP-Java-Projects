30984e4795ab458f4bb4ef6dbb00aaa8ca4bc057
elkhatib.h1 <elkhatib.h1@stu.janesville.k12.wi.us>
2022-09-26 10:08:07 -0500

    update

---
+++ src/LoopTest.java
@@ -0,0 +1,16 @@
+public class LoopTest {
+    public static void main(String[] args) {
+        int lcv = 1;
+        while (lcv <= 10) {
+            System.out.print(lcv + " ");
+            lcv++;
+        }
+        System.out.println();
+
+        // ONLY USE FOR LOOPS WITH LOOPS THAT HAVE A COUNTER
+        for (int lcv2 = 1; lcv2 <= 10; lcv2++) {
+            System.out.print(lcv2 + " ");
+        }
+        System.out.println();
+    }
+}
